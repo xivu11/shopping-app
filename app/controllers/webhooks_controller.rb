4f5fcdfb71b8cc7661a86d7412ba632a79adbadc
xivu11 <shivani.chhajed@protonshub.in>
2021-12-09 13:23:41 +0530

    First commit

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
     def create
+        puts "CALLLEEDdddddddddddddddddddddddddddddddddd"
+        #redirected_to :page_pth, notice: 'ssus placed'
     end
 end
